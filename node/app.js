c69f413cc85e42cf355c79b2df06ccb39b3b6658
Mengqi Zong <zongmengqi@gmail.com>
2017-10-07 14:44:44 -0700

    ziltex link version

--- node/app.js
+++ node/app.js
@@ -15,7 +15,7 @@ const client = redis.createClient('6379', 'redis');
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res, next) {
+app.get('/count', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
     res.send('This page has been viewed ' + counter + ' times!');
